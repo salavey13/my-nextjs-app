1186716d59e65f8cdb9d7823ff7f633f5fe397ff
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-08-22 03:51:42 +0300

    dark theme disabled for now, too dark4 default8)

--- components/ThemeHandler.tsx
+++ components/ThemeHandler.tsx
@@ -5,29 +5,29 @@
 import { useEffect } from "react";
 
 const ThemeHandler = () => {
-  useEffect(() => {
-    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+//   useEffect(() => {
+//     const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    if (prefersDarkScheme) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
+//     if (prefersDarkScheme) {
+//       document.documentElement.classList.add("dark");
+//     } else {
+//       document.documentElement.classList.remove("dark");
+//     }
 
-    const themeChangeListener = (e: MediaQueryListEvent) => {
-      if (e.matches) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
-    };
+//     const themeChangeListener = (e: MediaQueryListEvent) => {
+//       if (e.matches) {
+//         document.documentElement.classList.add("dark");
+//       } else {
+//         document.documentElement.classList.remove("dark");
+//       }
+//     };
 
-    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", themeChangeListener);
+//     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", themeChangeListener);
 
-    return () => {
-      window.matchMedia("(prefers-color-scheme: dark)").removeEventListener("change", themeChangeListener);
-    };
-  }, []);
+//     return () => {
+//       window.matchMedia("(prefers-color-scheme: dark)").removeEventListener("change", themeChangeListener);
+//     };
+//   }, []);
 
   return null;
 };
