9da858c4adc94d4b433a1af548698849b088a38d
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-12 02:54:17 +0300

    Update enhancedChatButton.tsx fix

--- components/ui/enhancedChatButton.tsx
+++ components/ui/enhancedChatButton.tsx
@@ -50,7 +50,7 @@ const EnhancedChatButton = () => {
     <div className={`flex items-center justify-center ${theme === 'dark' ? 'bg-black' : 'bg-white'} p-4 gap-8`}>
       <Button
         onClick={handleOpenVPN}
-        className="group flex flex-col items-center justify-center text-gray-400 w-36 h-24 gap-2 hover:text-blue-500 transition-all duration-300 rounded-lg transform hover:scale-110"
+        className="group flex flex-col items-center justify-center text-gray-400 w-36 h-18 gap-2 hover:text-blue-500 transition-all duration-300 rounded-lg transform hover:scale-110"
       >
         <svg className="w-10 h-10 text-gray-400 group-hover:text-blue-500 transition-all duration-300" version="1.0" xmlns="http://www.w3.org/2000/svg"
  width="192.000000pt" height="192.000000pt" viewBox="0 0 192.000000 192.000000"
@@ -111,7 +111,7 @@ fill="#FFFFFF" stroke="none">
       </Button>
       <Button
         onClick={handleOpenVercel}
-        className="group flex flex-col items-center justify-center text-gray-400 w-36 h-24 gap-2 hover:text-blue-500 transition-all duration-300 rounded-lg transform hover:scale-110"
+        className="group flex flex-col items-center justify-center text-gray-400 w-36 h-18 gap-2 hover:text-blue-500 transition-all duration-300 rounded-lg transform hover:scale-110"
       >
          <svg
           className="w-10 h-10 text-gray-400 group-hover:text-blue-500 transition-all duration-300"
@@ -121,15 +121,15 @@ fill="#FFFFFF" stroke="none">
           viewBox="0 0 283 64"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <span className="text-xs mb-1 transition-all duration-300 group-hover:text-blue-500">Vercel</span>
-        
+          
           <path 
           fill="#FFFFFF" strokeLinecap="round" strokeLinejoin="round" d="M141 16c-11 0-19 7-19 18s9 18 20 18c7 0 13-3 16-7l-7-5c-2 3-6 4-9 4-5 0-9-3-10-7h28v-3c0-11-8-18-19-18zm-9 15c1-4 4-7 9-7s8 3 9 7h-18zm117-15c-11 0-19 7-19 18s9 18 20 18c6 0 12-3 16-7l-8-5c-2 3-5 4-8 4-5 0-9-3-11-7h28l1-3c0-11-8-18-19-18zm-10 15c2-4 5-7 10-7s8 3 9 7h-19zm-39 3c0 6 4 10 10 10 4 0 7-2 9-5l8 5c-3 5-9 8-17 8-11 0-19-7-19-18s8-18 19-18c8 0 14 3 17 8l-8 5c-2-3-5-5-9-5-6 0-10 4-10 10zm83-29v46h-9V5h9zM37 0l37 64H0L37 0zm92 5-27 48L74 5h10l18 30 17-30h10zm59 12v10l-3-1c-6 0-10 4-10 10v15h-9V17h9v9c0-5 6-9 13-9z"></path>
         </svg>
+       <span className="text-xs mb-1 transition-all duration-300 group-hover:text-blue-500">Vercel</span> 
       </Button>
       <Button
         onClick={handleOpenGitHub}
-        className="group flex flex-col items-center justify-center text-gray-400 w-36 h-24 gap-2 hover:text-blue-500 transition-all duration-300 rounded-lg transform hover:scale-110"
+        className="group flex flex-col items-center justify-center text-gray-400 w-36 h-18 gap-2 hover:text-blue-500 transition-all duration-300 rounded-lg transform hover:scale-110"
       >
          <svg className="w-10 h-10 text-gray-400 group-hover:text-blue-500 transition-all duration-300" width="800px" height="800px" viewBox="0 -3.5 256 256" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet">
 
