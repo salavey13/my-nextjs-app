f464909f3149e8821551269a89b4a04dcd04e0b0
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-12 17:02:29 +0300

    Update page.tsx about removed to profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,10 @@
 //app\profile\page.tsx
 import Profile from "@/components/Profile";
-
+import AboutApp from '@/components/ui/AboutApp';
 export default function ProfilePage() {
-  return <Profile />;
+  return
+    <>
+     <Profile />;
+     <AboutApp />; 
+    </>
 }
