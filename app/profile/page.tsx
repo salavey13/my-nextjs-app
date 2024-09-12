22ee613c2e9e285953e1023979d85f32351ebe4b
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-12 17:08:40 +0300

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,8 +3,8 @@ import Profile from "@/components/Profile";
 import AboutApp from '@/components/ui/AboutApp';
 export default function ProfilePage() {
   return
-    <>
+    <div className="flex-grow" >
      <Profile />;
      <AboutApp />; 
-    </>
+    </div>
 }
