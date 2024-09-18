b917214a514e60976ca6e0291a09d77b58619ff2
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-18 03:19:52 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,7 +70,18 @@ const config: Config = {
               "to": {
                 "height": "0"
               }
-            }
+            },
+        "shine-pulse": {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
           }
 },
   plugins: [require("tailwindcss-animate")],
