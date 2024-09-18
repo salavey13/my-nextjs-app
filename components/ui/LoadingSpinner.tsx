9211f5ab0bfe9dde5f263aa10ed6d15440bcfd8b
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-18 03:29:14 +0300

    Update LoadingSpinner.tsx

--- components/ui/LoadingSpinner.tsx
+++ components/ui/LoadingSpinner.tsx
@@ -1,31 +1,43 @@
 // components/ui/LoadingSpinner.tsx
 "use client"
+import { cn } from "@/lib/utils";
 
-import React, { useEffect } from "react";
-import useTelegram from "@/hooks/useTelegram";
-
-const LoadingSpinner = () => {
-  const { showProgress } = useTelegram();
-
-  useEffect(() => {
-    // Show the native Telegram progress bar when the component is mounted
-    showProgress(true);
-
-    // Optionally, hide the progress bar when the component is unmounted
-    return () => {
-      showProgress(false);
-    };
-  }, [showProgress]);
-
+export default function LoadingSpinner({
+  className,
+  angle = 65,
+}: {
+  className?: string;
+  angle?: number;
+}) {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gradient-to-b from-gray-900 via-black to-gray-900">
-      <div className="text-center">
-        <div className="loader border-4 border-t-4 border-gray-400 h-16 w-16 mb-4 animate-spin rounded-full"></div>
-        {/* <p className="text-xl text-gray-200 neon-glow">Loading...</p> */}
+    <div
+      className={cn(
+        "pointer-events-none absolute size-full overflow-hidden opacity-50 [perspective:200px]",
+        className,
+      )}
+      style={{ "--grid-angle": `${angle}deg` } as React.CSSProperties}
+    >
+      {/* Grid */}
+      <div className="absolute inset-0 [transform:rotateX(var(--grid-angle))]">
+        <div
+          className={cn(
+            "animate-grid",
+
+            "[background-repeat:repeat] [background-size:60px_60px] [height:300vh] [inset:0%_0px] [margin-left:-50%] [transform-origin:100%_0_0] [width:600vw]",
+
+            // Light Styles
+            "[background-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0,0.3)_1px,transparent_0)]",
+
+            // Dark styles
+            "dark:[background-image:linear-gradient(to_right,rgba(255,255,255,0.2)_1px,transparent_0),linear-gradient(to_bottom,rgba(255,255,255,0.2)_1px,transparent_0)]",
+          )}
+        />
       </div>
+
+      {/* Background Gradient */}
+      <div className="absolute inset-0 bg-gradient-to-t from-white to-transparent to-90% dark:from-black" />
     </div>
   );
-};
+}
 
-export default LoadingSpinner;
 
