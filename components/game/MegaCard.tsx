c5955907cee3b04d5dbf7efb52556c60a900feb8
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-19 20:04:11 +0300

    Update MegaCard.tsx rrr

--- components/game/MegaCard.tsx
+++ components/game/MegaCard.tsx
@@ -177,7 +177,7 @@ export const MegaCard: React.FC<MegaCardProps> = React.memo(({ card, onCardUpdat
           position: 'absolute',
           top: 0,
           left: 0,
-          contentVisibility: `${card.flipped? 'hidden' : ''}`,
+          contentVisibility: `${(card.flipped || forceFlipped )? 'hidden' : 'visible'}`,
           transform: to([rotY], (r) => `rotateY(${r + 180}deg)`),
         }}
       />
