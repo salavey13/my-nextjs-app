68536242a4c5d0e691f161f42989049bc2bb4007
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-23 23:01:54 +0300

    Update DiceGame.tsx magic

--- components/game/DiceGame.tsx
+++ components/game/DiceGame.tsx
@@ -190,7 +190,7 @@ function Scene({ gameState, onRollComplete, wallTexture }: { gameState: GameStat
       break;
     default:
       // Default portrait
-      setGyro({ x: (z as number), y: (x as number), z: (y as number) });
+      setGyro({ x: (z as number), y: (y as number), z: (x as number) });
       break;
   }
 };
