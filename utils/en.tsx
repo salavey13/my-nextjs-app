1622d59b61ed11db3f95cbe0d7380d42ac5ba4b5
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-09-26 03:56:59 +0300

    Update en.tsx

--- utils/en.tsx
+++ utils/en.tsx
@@ -1,5 +1,6 @@
 import { LanguageDictionary } from "../utils/TranslationUtils";
 export const en: LanguageDictionary = {
+  backToCreators:"Back to Creators",
   pleaseGrantAccess: "Please grant access to your camera and microphone to play the game.",
   checkPermissions: "Check Permissions",
   "yeetCoefficient": "Yeet Coefficient",
