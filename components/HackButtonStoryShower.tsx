0c215ba8dad6f886ebeeabb4537f470d7f383b53
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-10-04 22:05:07 +0300

    Update HackButtonStoryShower.tsx

--- components/HackButtonStoryShower.tsx
+++ components/HackButtonStoryShower.tsx
@@ -405,8 +405,8 @@ export default function HackButtonStoryShower() {
   )
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-[calc(100vh-64px)] overflow-y-auto">
-      {/* <div className="absolute inset-0 z-0">
+    <div className="game-board h-[calc(100vh-64px)] pt-16 relative overflow-hidden">
+        {/* <div className="absolute inset-0 z-0">
           <InfinityMirror layers={15} baseColor="#282c33" accentColor="#e1ff01" />
         </div> */}
       {showCrashSimulation && (
