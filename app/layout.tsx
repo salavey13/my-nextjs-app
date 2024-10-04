d61078fb40a282392af3353005efd286538c5cf2
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-10-04 19:46:42 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,8 +31,8 @@ export default function RootLayout({
               <PageWrapper>
                 {children}
               </PageWrapper>
+              <GlitchyToastProvider />
             </main>
-            <GlitchyToastProvider />
           </AppProvider>
         </Suspense>
       </body>
