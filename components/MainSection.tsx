6c431a20f1591e56f4c5f1314897cc9fb7cdfae7
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-10-05 15:17:32 +0300

    Update MainSection.tsx

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -119,6 +119,7 @@ export default function MainSection({ setItemDetailsModalOpen, setSelectedItem, 
     <div className="w-full min-h-[calc(100vh-128px)]">
       {showOnboarding ? (
         <OnboardingSection />
+        <div className="w-full min-h-[calc(100vh-64px)]"/>
       ) : !selectedCreator ? (
         <div className="grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
           {creators.map((creator) => (
