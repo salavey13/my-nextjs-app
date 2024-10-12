9017e7caf2a31eef2d936d60368b048d1942d55a
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-10-12 23:52:50 +0300

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -11,6 +11,7 @@ import { ChevronRight, Code, Menu, Palette, X, Zap, Globe, Lock, Users } from "l
 import { motion, AnimatePresence, useScroll, useMotionValueEvent } from "framer-motion"
 import Image from "next/image"
 import { toast } from "@/hooks/use-toast"
+import { useAppContext } from '../context/AppContext';
 
 const socialLinks = [
   { name: "YouTube", icon: "M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z", url: "https://youtube.com/salavey13" },
@@ -25,6 +26,7 @@ const socialLinks = [
 ]
 
 export default function LandingPage() {
+  const { t } = useAppContext()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const [activeSection, setActiveSection] = useState("")
   const [isNavbarVisible, setIsNavbarVisible] = useState(true)
