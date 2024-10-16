77b48e752c13400462fcfceed172a0bdc486a3d7
salavey13 <135398606+salavey13@users.noreply.github.com>
2024-10-16 13:07:09 +0300

    Update bottomShelf.tsx

--- components/ui/bottomShelf.tsx
+++ components/ui/bottomShelf.tsx
@@ -22,26 +22,26 @@ interface BottomShelfProps {
 
 const BottomShelf: React.FC<BottomShelfProps> = ({ bitmask }) => {
   const pathname = usePathname()
-  const { state } = useAppContext()
+  const { state, t } = useAppContext()
   const user = state.user
 
   const navigationLinks: NavigationLink[] = [    
-    { href: '/paymentnotification', icon: <DollarSign className="w-6 h-6" />, label: 'Payment Notification', stageMask: 0b11111000, component: 'paymentNotification' },
-    { href: '/dynamicitemform', icon: <List className="w-6 h-6" />, label: 'Dynamic Item Form', stageMask: 0b11111000, component: 'dynamicItemForm' },
-    { href: '/qrcodeform', icon: <Zap className="w-6 h-6" />, label: 'QR Code Form', stageMask: 0b11111000, component: 'qrCodeForm' },
-    { href: '/cryptopayment', icon: <ShoppingCart className="w-6 h-6" />, label: 'Crypto Payment', stageMask: 0b11111000, component: 'cryptoPayment' },
-    { href: '/', icon: <Home className="w-6 h-6" />, label: 'Home', stageMask: 0b11111111 },
-    { href: '/rent', icon: <Car className="w-6 h-6" />, label: 'Rent', stageMask: 0b11111000, component: 'rent' },
-    { href: '/referral', icon: <Users className="w-6 h-6" />, label: 'Referral', stageMask: 0b11111000, component: 'referral' },
-    { href: '/profile', icon: <User className="w-6 h-6" />, label: 'Profile', stageMask: 0b11111111 },
-    { href: '/questsforcoins', icon: <Dice1 className="w-6 h-6" />, label: '!', stageMask: 0b11111000, component: 'questsForCoins' },
-    { href: '/bets', icon: <Zap className="w-6 h-6" />, label: 'IQ', stageMask: 0b11111000, component: 'bets' },
-    { href: '/quiz', icon: <ZapOff className="w-6 h-6" />, label: 'Quiz', stageMask: 0b11111111, component: 'quiz' },
-    { href: '/createEvent', icon: <CalendarPlus className="w-6 h-6" />, label: 'Create Event', stageMask: 0b11111000, component: 'createEvent' },
-    { href: '/conflictawareness', icon: <Globe className="w-6 h-6" />, label: 'Conflict Awareness', stageMask: 0b11111000,   component: 'conflictAwareness' },
-    { href: '/admin', icon: <Crown className="w-6 h-6" />, label: 'Admin', stageMask: 0b10000000, component: 'admin' },
-    { href: '/dev', icon: <Lightbulb className="w-6 h-6" />, label: 'Dev', stageMask: 0b11000000, component: 'dev' },
-    { href: '/versimcel', icon: <Gamepad className="w-6 h-6" />, label: 'VersimCel', stageMask: 0b11111000, component: 'versimcel' },
+    { href: '/paymentnotification', icon: <DollarSign className="w-6 h-6" />, label: t('bottomShelf.paymentNotification'), stageMask: 0b11111000, component: 'paymentNotification' },
+    { href: '/dynamicitemform', icon: <List className="w-6 h-6" />, label: t('bottomShelf.dynamicItemForm'), stageMask: 0b11111000, component: 'dynamicItemForm' },
+    { href: '/qrcodeform', icon: <Zap className="w-6 h-6" />, label: t('bottomShelf.qrCodeForm'), stageMask: 0b11111000, component: 'qrCodeForm' },
+    { href: '/cryptopayment', icon: <ShoppingCart className="w-6 h-6" />, label: t('bottomShelf.cryptoPayment'), stageMask: 0b11111000, component: 'cryptoPayment' },
+    { href: '/', icon: <Home className="w-6 h-6" />, label: t('bottomShelf.home'), stageMask: 0b11111111 },
+    { href: '/rent', icon: <Car className="w-6 h-6" />, label: t('bottomShelf.rent'), stageMask: 0b11111000, component: 'rent' },
+    { href: '/referral', icon: <Users className="w-6 h-6" />, label: t('bottomShelf.referral'), stageMask: 0b11111000, component: 'referral' },
+    { href: '/profile', icon: <User className="w-6 h-6" />, label: t('bottomShelf.profile'), stageMask: 0b11111111 },
+    { href: '/questsforcoins', icon: <Dice1 className="w-6 h-6" />, label: t('bottomShelf.quests'), stageMask: 0b11111000, component: 'questsForCoins' },
+    { href: '/bets', icon: <Zap className="w-6 h-6" />, label: t('bottomShelf.bets'), stageMask: 0b11111000, component: 'bets' },
+    { href: '/quiz', icon: <ZapOff className="w-6 h-6" />, label: t('bottomShelf.quiz'), stageMask: 0b11111111, component: 'quiz' },
+    { href: '/createEvent', icon: <CalendarPlus className="w-6 h-6" />, label: t('bottomShelf.createEvent'), stageMask: 0b11111000, component: 'createEvent' },
+    { href: '/conflictawareness', icon: <Globe className="w-6 h-6" />, label: t('bottomShelf.conflictAwareness'), stageMask: 0b11111000, component: 'conflictAwareness' },
+    { href: '/admin', icon: <Crown className="w-6 h-6" />, label: t('bottomShelf.admin'), stageMask: 0b10000000, component: 'admin' },
+    { href: '/dev', icon: <Lightbulb className="w-6 h-6" />, label: t('bottomShelf.dev'), stageMask: 0b11000000, component: 'dev' },
+    { href: '/versimcel', icon: <Gamepad className="w-6 h-6" />, label: t('bottomShelf.versimcel'), stageMask: 0b11111000, component: 'versimcel' },
   ]
 
   return (
